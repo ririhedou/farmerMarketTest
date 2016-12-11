3da6bc198c63d8df36189006c182ebe30e95589c
Ke Tian <ketian@kes-MacBook-Air.local>
2016-12-11 14:56:30 -0500

    ketian

--- build/generated-sources/ap-source-output/market/mymarket/entityclasses/Farm_.java
+++ build/generated-sources/ap-source-output/market/mymarket/entityclasses/Farm_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import market.mymarket.entityclasses.Category;
 import market.mymarket.entityclasses.Goodslist;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-11T14:05:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-11T14:55:39")
 @StaticMetamodel(Farm.class)
 public class Farm_ { 
 
