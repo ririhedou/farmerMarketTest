990251efdb211affadcdbf892890d314eb353117
Ke Tian <ketian@kes-MacBook-Air.local>
2016-12-12 13:37:41 -0500

    kt

--- build/generated-sources/ap-source-output/market/mymarket/entityclasses/Customer_.java
+++ build/generated-sources/ap-source-output/market/mymarket/entityclasses/Customer_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import market.mymarket.entityclasses.Photo;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-12T11:48:45")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-12T13:30:31")
 @StaticMetamodel(Customer.class)
 public class Customer_ { 
 
